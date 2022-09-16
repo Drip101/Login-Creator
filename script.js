24f8916a5f552e2221655ab24d2709c2ddc81f7b
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-09-16 15:51:41 -0400

    added user input window for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+  var userInput = window.prompt("How long would you like your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (is NaN(passwordLength)) {
+    window.alert("This is not a number!")
+    return
+  } 
+
+  if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Password length must be between 8 and 128 characters")
+return
+}
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
